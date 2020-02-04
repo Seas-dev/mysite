a7b7571c189646ef452285ff2a1cdb6a7993b00f
Seas-dev <seastedt.ej@gmail.com>
2020-02-04 15:23:54 -0500

    Updates

---
+++ precache-manifest.db7a1640003e68784baf046af7067c00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "391fc01740f89b25e0b0b1f6479e6904",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "6dfbf2b40abbfda7ed72",
+    "url": "/mysite/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "51f5385e676daa02a835",
+    "url": "/mysite/static/css/main.afcaac5e.chunk.css"
+  },
+  {
+    "revision": "6dfbf2b40abbfda7ed72",
+    "url": "/mysite/static/js/2.c96f6af0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mysite/static/js/2.c96f6af0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51f5385e676daa02a835",
+    "url": "/mysite/static/js/main.859c2bcf.chunk.js"
+  },
+  {
+    "revision": "86af423b708807494c89",
+    "url": "/mysite/static/js/runtime-main.25962889.js"
+  }
+]);

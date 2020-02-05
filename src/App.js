7456dae7338e2f04f32f82e9dc39d8eff913156f
Seas-dev <seastedt.ej@gmail.com>
2020-02-04 20:43:17 -0500

    finally got the routing to work, took some research and some help from createHistory, createMemorySource, and LocationProvider from @reach/router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Components/Header';
 import Nav from './Components/Nav';
 import About from './Components/About';
-import { Router } from '@reach/router';
+import { Router, createHistory, createMemorySource, LocationProvider } from '@reach/router';
 import Skills from './Components/Skills';
 import Projects from './Components/Projects';
 import Footer from './Components/Footer';
@@ -10,17 +10,22 @@ import Contact from './Components/Contact';
 
 function App() {
 
+  let source = createMemorySource('/mysite/');
+  let history = createHistory(source);
+
   return (
     <div className="container">
-      <Header />
-      <Nav />
-      <Router>
-        <About path="/mysite/." />
-        <Skills path="/mysite/skills" />
-        <Projects path="/mysite/projects"/>
-        <Contact path="/mysite/contactme" />
-      </Router>
-      <Footer />
+      <LocationProvider history={history}>
+        <Header />
+        <Nav />
+        <Router>
+          <About path="/mysite/" />
+          <Skills path="/mysite/skills" />
+          <Projects path="/mysite/projects"/>
+          <Contact path="/mysite/contactme" />
+        </Router>
+        <Footer />
+      </LocationProvider>
     </div>
   );
 }
